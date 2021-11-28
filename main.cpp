8bc619c4e34d46e58b769eb83acafe5e0654282a
Test <Test@test.com>
2021-11-27 22:28:58 -0500

    Init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Tyler Coleman
+ */
+
+/*
+* Quadratic equations are of the form
+* a · x
+* 2 + b · x + c = 0
+* To solve these, one uses the quadratic formula:
+* There is a problem, though: if b2–4ac is less than zero, then it will fail. Write a program that can calculate x for a
+* quadratic equation. Create a function that prints out the roots of a quadratic equation, given a, b, c. When the program
+* detects an equation with no real roots, have it print out a message. How do you know that your results are plausible? Can
+* you check that they are correct?
+*/
+

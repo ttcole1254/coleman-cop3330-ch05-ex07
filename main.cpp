468a8ffaa91045fa077b6418279c5caf9dc87076
Test <Test@test.com>
2021-11-29 23:38:47 -0500

    Final

--- main.cpp
+++ main.cpp
@@ -14,3 +14,55 @@
 * you check that they are correct?
 */
 
+#include "std_lib_facilities.h"	
+
+const double epsilon = 1.0e-7;
+double found(double a, double b, double c, double x) {
+    double e = a * x * x + b * x + c;
+    if (e == 0) return x;
+    if (0 < e && epsilon < e) {
+        cout << "The root is off by: " << e << "\n";
+    } else if (e < -epsilon) {
+        cout << "The root is off by: " << e << "\n";
+    }
+    return x;
+}
+
+int main()
+{
+    try {
+        double a, b, c;
+        cout << "Quadratic Equation Solver: Enter three numbers separated by spaces (ex: A B C) to solve for (a * x * x) + (b * x) + c == 0: ";
+        while (cin >> a >> b >> c) {
+            if (a == 0) {
+                if (b == 0) {
+                    cout << "There is no root.\n";
+                } else {
+                    cout << "x == " << found(a, b, c, -c / b) << "\n";
+                }
+            } else if (b == 0) {
+                double ca = -c / a;
+                if (ca == 0) {
+                    cout << "x == 0\n";
+                } else if (ca < 0) {
+                    cout << "There are no real roots for the numbers given.\n";
+                } else {
+                    cout << "There are two real roots: " << found(a, b, c, sqrt(ca)) << " and " << found(a, b, c, -sqrt(ca)) << "\n";
+                }
+            } else {
+                double sq = b * b - 4 * a * c;
+                if (sq == 0) {
+                    cout << "There is one real root: " << found(a, b, c, -b / (2 * a)) << "\n";
+                } else if (sq < 0) {
+                    cout << "There are no real roots for the numbers given.\n";
+                } else {
+                    cout << "There are two real roots: " << found(a, b, c, (-b + sqrt(sq)) / (2 * a)) << " and " << found(a, b, c, (-b - sqrt(sq)) / (2 * a)) << "\n";
+                }
+            }
+            cout << "Please try again (enter a b c): ";
+        }
+    }
+    catch (runtime_error e) {
+        cout << e.what() << "\n";
+    }
+}
